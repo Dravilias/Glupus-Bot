292db366f22c1dd51d00d0321b96d9b368aa7fd8
Dravilias <kubagalatpriv@gmail.com>
2026-03-07 17:01:50 +0100

    config.py cleanup

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
-# lowk im not sharing this so so get it urself from discord.com/developers
-import os
 
+import os
+# this safely hidden im not a retard
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 GLUP_EMOJI = "<:glup:1470514577262510132>"
-
-GLUP_GIF = "https://cdn.discordapp.com/attachments/1208121918855577700/1358147163380645888/glup.gif?ex=699aa2dc&is=6999515c&hm=a44fd025a67a6f36ef414e7a5157b5067cd08bb4811dd0755e9f951911a03a4e&"
 
 GLUP_STICKER = 1470421098410807501
